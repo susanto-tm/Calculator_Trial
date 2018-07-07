e2559afc3c3091579aea868ce3cb812b01b26ca3
susanto-tm <37474291+susanto-tm@users.noreply.github.com>
2018-07-07 17:05:01 +0700

    Calculator UI Setup

--- app1/ViewController.designer.cs
+++ app1/ViewController.designer.cs
@@ -16,31 +16,157 @@ namespace app1
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton CallButton { get; set; }
+        UIKit.UIButton Button0 { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UITextField PhoneNumberText { get; set; }
+        UIKit.UIButton Button1 { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton TranslateButton { get; set; }
+        UIKit.UIButton Button2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button3 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button4 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button5 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button6 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button7 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button8 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Button9 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ButtonDivide { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ButtonEqual { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ButtonMinus { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ButtonPlus { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ButtonTimes { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ClearField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField DisplayValue { get; set; }
 
         void ReleaseDesignerOutlets ()
         {
-            if (CallButton != null) {
-                CallButton.Dispose ();
-                CallButton = null;
+            if (Button0 != null) {
+                Button0.Dispose ();
+                Button0 = null;
             }
 
-            if (PhoneNumberText != null) {
-                PhoneNumberText.Dispose ();
-                PhoneNumberText = null;
+            if (Button1 != null) {
+                Button1.Dispose ();
+                Button1 = null;
             }
 
-            if (TranslateButton != null) {
-                TranslateButton.Dispose ();
-                TranslateButton = null;
+            if (Button2 != null) {
+                Button2.Dispose ();
+                Button2 = null;
+            }
+
+            if (Button3 != null) {
+                Button3.Dispose ();
+                Button3 = null;
+            }
+
+            if (Button4 != null) {
+                Button4.Dispose ();
+                Button4 = null;
+            }
+
+            if (Button5 != null) {
+                Button5.Dispose ();
+                Button5 = null;
+            }
+
+            if (Button6 != null) {
+                Button6.Dispose ();
+                Button6 = null;
+            }
+
+            if (Button7 != null) {
+                Button7.Dispose ();
+                Button7 = null;
+            }
+
+            if (Button8 != null) {
+                Button8.Dispose ();
+                Button8 = null;
+            }
+
+            if (Button9 != null) {
+                Button9.Dispose ();
+                Button9 = null;
+            }
+
+            if (ButtonDivide != null) {
+                ButtonDivide.Dispose ();
+                ButtonDivide = null;
+            }
+
+            if (ButtonEqual != null) {
+                ButtonEqual.Dispose ();
+                ButtonEqual = null;
+            }
+
+            if (ButtonMinus != null) {
+                ButtonMinus.Dispose ();
+                ButtonMinus = null;
+            }
+
+            if (ButtonPlus != null) {
+                ButtonPlus.Dispose ();
+                ButtonPlus = null;
+            }
+
+            if (ButtonTimes != null) {
+                ButtonTimes.Dispose ();
+                ButtonTimes = null;
+            }
+
+            if (ClearField != null) {
+                ClearField.Dispose ();
+                ClearField = null;
+            }
+
+            if (DisplayValue != null) {
+                DisplayValue.Dispose ();
+                DisplayValue = null;
             }
         }
     }
